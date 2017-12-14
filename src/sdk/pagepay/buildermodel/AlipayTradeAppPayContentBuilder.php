c7526cc2701e2de62bdb5b01fbbecf3d3ac70ec0
Mr-sunK <1042080686@qq.com>
2017-12-14 11:24:19 +0800

    add pc app page pay

---
+++ src/sdk/pagepay/buildermodel/AlipayTradeAppPayContentBuilder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunku
+ * Date: 2017/12/14
+ * Time: 11:18
+ */
+
+namespace mrk\sdk\pagepay\buildermodel;
+
+
+class AlipayTradeAppPayContentBuilder
+{
+
+}
